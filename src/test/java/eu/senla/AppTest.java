2bda4e29dcc63598cfae6dfc6d76bddcd7555468
vadim <vadim.linkleon@gmail.com>
2025-04-15 22:14:11 +0300

    Initial commit

---
+++ src/test/java/eu/senla/AppTest.java
@@ -0,0 +1,10 @@
+package eu.senla;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Unit test for simple App. */
+public class AppTest {
+  public void testApp() {
+    assertEquals(4, 2+2);
+  }
+}
