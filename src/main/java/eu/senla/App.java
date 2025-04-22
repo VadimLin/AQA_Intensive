6e300c97e64beff3e7a279b7320f57f411e6d809
VadimLin <vadim.linkleon@gmail.com>
2025-04-22 18:00:02 +0300

    add suppression file; check on unused imports;add checkstyle for tests

--- src/main/java/eu/senla/App.java
+++ src/main/java/eu/senla/App.java
@@ -1,8 +1,14 @@
 package eu.senla;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /** Hello world! */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
+    WebDriver driver = new ChromeDriver();
+
+    driver.get("");
   }
 }
--- src/test/java/eu/senla/AppTest.java
+++ src/test/java/eu/senla/AppTest.java
@@ -1,10 +1,6 @@
 package eu.senla;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /** Unit test for simple App. */
 public class AppTest {
-  public void testApp() {
-    assertEquals(4, 2+2);
-  }
+  public void testApp() {}
 }
