1705f22796c54ca8d2e45f7290e30036f42cdee6
VadimLin <vadim.linkleon@gmail.com>
2025-08-19 15:43:37 +0300

    new

--- src/test/java/eu/senla/BaseTest.java
+++ src/test/java/eu/senla/BaseTest.java
@@ -1,9 +1,11 @@
 package eu.senla;
 
 import eu.senla.Driver.Driver;
+import eu.senla.Endpoints.Endpoints;
 import eu.senla.LoginPage.LoginPage;
 import eu.senla.LogoutPage.LogoutPage;
 import eu.senla.PropertyFile.ReadPropertyFile;
+import eu.senla.Registration.ApiLogin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
@@ -22,18 +24,18 @@ public class BaseTest {
   }
 
 
-//  @BeforeEach
-//  final void apiLogin() {
-//    init();
-//
-//    ApiLogin authenticate = new ApiLogin(Endpoints.MAIN_URL + Endpoints.WEB_EP + Endpoints.DASHBOARD_URL);
-//
-//    authenticate.login();
-//  }
-//
- // final void init() {
-  //  Driver.initializeDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-  //}
+  @BeforeEach
+  final void apiLogin() {
+    init();
+
+    ApiLogin authenticate = new ApiLogin(Endpoints.MAIN_URL + Endpoints.WEB_EP + Endpoints.DASHBOARD_URL);
+
+    authenticate.login();
+  }
+
+  final void init() {
+    Driver.initializeDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  }
 
   @AfterEach
   void tearDown() {
