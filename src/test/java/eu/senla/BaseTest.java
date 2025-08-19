e6b39f16c7be70694c0bc8ac52f9cf895509f9b0
VadimLin <vadim.linkleon@gmail.com>
2025-08-19 14:39:39 +0300

    new refactoring for final task

--- src/test/java/eu/senla/BaseTest.java
+++ src/test/java/eu/senla/BaseTest.java
@@ -23,18 +23,18 @@ public class BaseTest {
     driver = Driver.initializeDriver();
   }
 
-  @BeforeEach
-  final void apiLogin() {
-    init();
-
-    ApiLogin authenticate = new ApiLogin(Endpoints.MAIN_URL + Endpoints.WEB_EP + Endpoints.DASHBOARD_URL);
-
-    authenticate.login();
-  }
-
-  final void init() {
-    Driver.initializeDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-  }
+//  @BeforeEach
+//  final void apiLogin() {
+//    init();
+//
+//    ApiLogin authenticate = new ApiLogin(Endpoints.MAIN_URL + Endpoints.WEB_EP + Endpoints.DASHBOARD_URL);
+//
+//    authenticate.login();
+//  }
+//
+//  final void init() {
+//    Driver.initializeDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//  }
 
   @AfterEach
   void tearDown() {
